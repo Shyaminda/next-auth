f60d0471d578710d8f351986c5d612ca725e0280
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2024-12-30 20:04:07 +0530

    add create user functionality

--- actions/register.ts
+++ actions/register.ts
@@ -2,6 +2,9 @@
 
 import { registerSchema } from "@/schemas";
 import { z } from "zod";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
+import { getUserByEmail } from "@/data/user";
 
 export const register = async (values: z.infer<typeof registerSchema>) => {
 	const validateFields = registerSchema.safeParse(values);
@@ -10,5 +13,22 @@ export const register = async (values: z.infer<typeof registerSchema>) => {
 		return { error: "Invalid fields" };
 	}
 
-	return { success: "Email sent" };
+	const { email, password, name } = validateFields.data;
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	const existingUser = await getUserByEmail(email);
+
+	if (existingUser) {
+		return { error: "User already exists" };
+	}
+
+	await db.user.create({
+		data: {
+			email,
+			password: hashedPassword,
+			name,
+		},
+	});
+
+	return { success: "User created" };
 };
