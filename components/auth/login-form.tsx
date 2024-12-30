59aeadc920eb95ec9c9fc3eff3c0d578c1a259e2
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2024-12-30 17:42:36 +0530

    add login form

--- components/auth/login-form.tsx
+++ components/auth/login-form.tsx
@@ -1,14 +1,84 @@
-import { CardWrapper } from "./card-wrapper";
+"use client";
+
+import { useForm } from "react-hook-form";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { loginSchema } from "@/schemas";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 export const LoginForm = () => {
+	const form = useForm<z.infer<typeof loginSchema>>({
+		resolver: zodResolver(loginSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+
+	const onSubmit = (values: z.infer<typeof loginSchema>) => {
+		console.log(values);
+	};
+
 	return (
 		<CardWrapper
 			headerLabel="Welcome back"
-			backButtonLabel="Dont have an account?"
+			backButtonLabel="Don't have an account?"
 			backButtonHref="/auth/register"
 			showSocial
 		>
-			Login Form
+			<Form {...form}>
+				<form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+					<div className="space-y-4">
+						<FormField
+							control={form.control}
+							name="email"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Email</FormLabel>
+									<FormControl>
+										<Input
+											{...field}
+											placeholder="john.doe@example.com"
+											type="email"
+										/>
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+						<FormField
+							control={form.control}
+							name="password"
+							render={({ field }) => (
+								<FormItem>
+									<FormLabel>Password</FormLabel>
+									<FormControl>
+										<Input {...field} placeholder="******" type="password" />
+									</FormControl>
+									<FormMessage />
+								</FormItem>
+							)}
+						/>
+					</div>
+					<FormError message="Invalid credentials" />
+					<FormSuccess message="Email sent" />
+					<Button type="submit" className="w-full">
+						Login
+					</Button>
+				</form>
+			</Form>
 		</CardWrapper>
 	);
 };
