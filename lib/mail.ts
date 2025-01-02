d7eca3ed11bb05b4843355f2ad382588c8c8a0d8
Shyaminda Senevirathna <shyamindasenevirathna@gmail.com>
2025-01-02 19:55:50 +0530

    add domain email

--- lib/mail.ts
+++ lib/mail.ts
@@ -6,7 +6,7 @@ export const sendVerificationEmail = async (email: string, token: string) => {
 	const confirmLink = `${process.env.NEXTAUTH_URL}/auth/new-verification?token=${token}`;
 
 	await resend.emails.send({
-		from: "onboarding@resend.dev",
+		from: "mail@elebot.pro",
 		to: email,
 		subject: "Confirm your email",
 		html: `<p>Click <a href="${confirmLink}">here</a> to confirm your email</p>`,
@@ -17,7 +17,7 @@ export const sendResetPasswordEmail = async (email: string, token: string) => {
 	const resetLink = `${process.env.NEXTAUTH_URL}/auth/new-password?token=${token}`;
 
 	await resend.emails.send({
-		from: "onboarding@resend.dev",
+		from: "mail@elebot.pro",
 		to: email,
 		subject: "Reset your password",
 		html: `<p>Click <a href="${resetLink}">here</a> to reset your password</p>`,
@@ -26,7 +26,7 @@ export const sendResetPasswordEmail = async (email: string, token: string) => {
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
 	await resend.emails.send({
-		from: "onboarding@resend.dev",
+		from: "mail@elebot.pro",
 		to: email,
 		subject: "2 factor authentication code",
 		html: `<p>Your 2FA code: ${token}</p>`,
